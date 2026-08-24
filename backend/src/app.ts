b7f691b372519665859126bfba55e3c1a96f70b5
FahdHamza47 <fahdhamza2011@gmail.com>
2026-08-24 04:40:16 +0300

    cors update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,17 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://shopsphereapp.vercel.app",
+      "http://localhost:5173",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 // Health check route
